5d1a155a02615b96f1f0d116692591cd4ffe886c
SeongUk52 <rkfka7849@gmail.com>
2023-11-02 19:12:27 +0900

    feat(알고리즘): 백준 1800

--- Algorithm/SolvedProblem/데이크스트라/1800/1800.py
+++ Algorithm/SolvedProblem/데이크스트라/1800/1800.py
@@ -2,27 +2,32 @@ import heapq
 
 
 def dijkstra():
-    dists = {i: float('infinity') for i in dict1}
-    dists[1] = 0
-    q = [(0, 1, k)]
+    dists = {j:{i: float('infinity') for i in dict1} for j in range(k + 1)}
+    for i in range(k + 1):
+        dists[i][1] = 0
+    q = [(0, 1, 0)]
 
     while q:
         cd, cv, ck = heapq.heappop(q)
 
-        if cd > dists[cv]:
+        if cd > dists[ck][cv]:
             continue
 
         for nv, wt in dict1[cv].items():
             dist = max(cd, wt)
-            if dist < dists[nv]:
-                dists[nv] = dist
-                heapq.heappush(q, (dist, nv, k))
+            if dist < dists[ck][nv]:
+                for i in range(ck, k + 1):
+                    if dists[i][nv] > dist:
+                        dists[i][nv] = dist
+                heapq.heappush(q, (dist, nv, ck))
 
-        if ck > 0:
-            for nv, wt in dict1[cv].items():
-                if cd < dists[nv]:
-                    dists[nv] = cd
-                    heapq.heappush(q, (cd, nv, k - 1))
+        if ck < k:
+            for nv in dict1[cv].keys():
+                if cd < dists[ck + 1][nv]:
+                    for i in range(ck + 1, k + 1):
+                        if dists[i][nv] > cd:
+                            dists[i][nv] = cd
+                    heapq.heappush(q, (cd, nv, ck + 1))
 
     return dists
 
@@ -34,9 +39,7 @@ for _ in range(p):
     a, b, c = map(int, input().split())
     dict1[a][b] = c
     dict1[b][a] = c
-
-print(dict1)
-result = dijkstra()
+result = dijkstra()[k][n]
 if result == float('infinity'):
     print(-1)
 else:
