784f9ec1c5cd414b0fd9186429374b67e7d1a3dc
SeongUk52 <rkfka7849@gmail.com>
2023-12-22 12:33:44 +0900

    feat(알고리즘):백준 14430

---
+++ Algorithm/SolvedProblem/DP/실버/14430/14430.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        if arr[i - 1][j - 1] == 1:
+            dp[i][j] += 1
+
+print(dp[-1][-1])
