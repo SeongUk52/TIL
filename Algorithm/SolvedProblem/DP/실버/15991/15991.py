4bbdf79d39002097c0ab3f15e60232d48f3e7aa1
SeongUk52 <rkfka7849@gmail.com>
2024-02-02 13:52:52 +0900

    feat(알고리즘):백준 15991

---
+++ Algorithm/SolvedProblem/DP/실버/15991/15991.py
@@ -0,0 +1,7 @@
+dp = [1, 1, 2, 2, 3, 3]
+for i in range(6, 100001):
+    dp.append((dp[i - 2] + dp[i - 4] + dp[i - 6]) % 1000000009)
+
+for _ in range(int(input())):
+    n = int(input())
+    print(dp[n])
