ca24f3c8b12fe3677d93d49c8a1615417a2d7fc1
SeongUk52 <rkfka7849@gmail.com>
2024-04-28 09:33:55 +0900

    feat(알고리즘):백준 1083

---
+++ Algorithm/SolvedProblem/그리디/1083/1083.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int, input().split()))
+s = int(input())
+
+for i in range(n - 1):
+    if s <= 0:
+        break
+    mx, idx = arr[i], i
+    for j in range(i + 1, min(n, i + 1 + s)):
+        if mx < arr[j]:
+            mx = arr[j]
+            idx = j
+    s -= idx - i
+    for j in range(idx, i ,-1):
+        arr[j] = arr[j - 1]
+    arr[i] = mx
+print(*arr)
