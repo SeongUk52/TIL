d53aa3e46c7c2f5d258b7ba3d46d516a6bdd3042
SeongUk52 <rkfka7849@gmail.com>
2024-05-14 12:56:54 +0900

    feat(알고리즘): 백준 1918

---
+++ Algorithm/SolvedProblem/자료구조/스택/1918/1918.py
@@ -0,0 +1,26 @@
+ll = list(input())
+stack = []
+result = ''
+
+for i in ll:
+    if i.isalpha():
+        result += i
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(i)
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(i)
+        elif i == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+while stack:
+    result += stack.pop()
+
+print(result)
