9296add46065beaf3a509e67ba437975efd90a34
SeongUk52 <rkfka7849@gmail.com>
2024-06-10 09:04:27 +0900

    feat(알고리즘): 백준 29808

---
+++ Algorithm/SolvedProblem/랜덤마라톤/코스001/29808/29808.py
@@ -0,0 +1,25 @@
+cnt = 0
+s = int(input())
+result = []
+if s % 4763 == 0:
+    s //= 4763
+    for i in range(401):
+        for j in range(401):
+            if i < 200:
+                r1 = i
+                a = i * 508
+            else:
+                r1 = i - 200
+                a = r1 * 108
+            if j < 200:
+                r2 = j
+                b = j * 212
+            else:
+                r2 = j - 200
+                b = r2 * 305
+            if s == a + b:
+                result.append([r1, r2])
+                cnt += 1
+print(cnt)
+for i in result:
+    print(*i)
