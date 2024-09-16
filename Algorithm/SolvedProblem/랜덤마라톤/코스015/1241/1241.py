7dd96ac7b66b4de6c2ce3b935da56e8444ea8454
SeongUk52 <rkfka7849@gmail.com>
2024-09-16 09:40:53 +0900

    feat(알고리즘): 백준 1241

--- Algorithm/SolvedProblem/랜덤마라톤/코스015/1241/1241.py
+++ Algorithm/SolvedProblem/랜덤마라톤/코스015/1241/1241.py
@@ -1,18 +1,26 @@
-N = int(input())
-student_nums = []
-for _ in range(N):
-    student_nums.append(int(input()))
-results = [0] * N
+import sys
+
+N = int(sys.stdin.readline())
+
+circle, result = [0] * N, [0] * N
 for i in range(N):
-    for j in range(i + 1, N):
-        stu1 = student_nums[i]
-        stu2 = student_nums[j]
-        if stu1 == stu2:
-            results[i] += 1
-            results[j] += 1
-        elif stu1 % stu2 == 0:
-            results[i] += 1
-        elif stu2 % stu1 == 0:
-            results[j] += 1
-for result in results:
-    print(result)
+    circle[i] = int(sys.stdin.readline())
+
+matrix = [0 for _ in range(max(circle) + 1)]
+for c in circle:
+    matrix[c] += 1
+
+for n in range(N):
+    k = 1
+    while k * k <= circle[n]:
+        if circle[n] % k == 0:
+            if k * k != circle[n]:
+                result[n] += matrix[k] + matrix[circle[n] // k]
+            else:
+                result[n] += matrix[k]
+        k += 1
+
+answer = ""
+for r in result:
+    answer += str(r - 1) + "\n"
+print(answer)
