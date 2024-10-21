e599e0be4ecb071057acbd463278e695b87ff0e3
SeongUk52 <rkfka7849@gmail.com>
2024-10-21 11:22:08 +0900

    feat: 백준 5639
    알고리즘 공부, 클래스 4번 문제

---
+++ Algorithm/SolvedProblem/백준클래스/클래스4/5639/5639.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+
+arr = []
+while True:
+    try:
+        x = int(input())
+        arr.append(x)
+    except:
+        break
+
+
+def solution(A):
+    if len(A) == 0:
+        return
+
+    tempL, tempR = [], []
+    mid = A[0]
+    for i in range(1, len(A)):
+        if A[i] > mid:
+            tempL = A[1:i]
+            tempR = A[i:]
+            break
+    else:
+        tempL = A[1:]
+
+    solution(tempL)
+    solution(tempR)
+    print(mid)
+
+
+solution(arr)
