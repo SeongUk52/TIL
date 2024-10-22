57221b8677d19f84ffa963026365e35cd4675b22
SeongUk52 <rkfka7849@gmail.com>
2024-10-22 10:29:27 +0900

    feat: 백준 30805

---
+++ Algorithm/SolvedProblem/백준클래스/클래스4/30805/30805.py
@@ -0,0 +1,30 @@
+def sol(arr1, arr2, res=None):
+    if res is None:
+        res = []
+    if (not arr1) or (not arr2):
+        return res
+
+    tmp1, tmp2 = max(arr1), max(arr2)
+    idx1, idx2 = arr1.index(tmp1), arr2.index(tmp2)
+
+    if tmp1 == tmp2:
+        res.append(tmp1)
+        return sol(arr1[idx1 + 1:], arr2[idx2 + 1:], res)
+    elif tmp1 > tmp2:
+        arr1.pop(idx1)
+        return sol(arr1, arr2, res)
+    else:
+        arr2.pop(idx2)
+        return sol(arr1, arr2, res)
+
+
+n = int(input())
+arr1 = list(map(int, input().split()))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+ans = sol(arr1, arr2)
+
+print(len(ans))
+if ans:
+    print(*ans)
